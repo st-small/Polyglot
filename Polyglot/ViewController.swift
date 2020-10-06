59d14e26f2598a1193a153c487ab289c81d88e9f
Станислав Шияновский <st-small@rambler.ru>
2020-10-06 10:28:49 +0300

    Initial Commit

---
+++ Polyglot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Polyglot
+//
+//  Created by Stanly Shiyanovskiy on 06.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
